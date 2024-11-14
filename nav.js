2e3949354c3c02a4227832d7f1e92f261ffd9757
Arun <arunneupane0000@gmail.com>
2024-11-14 13:54:53 +0545

    removed unused nav , updated ui, background colors, nav and perfected power mod , removed unnecessary padding and overall improved significantly

--- nav.js
+++ nav.js
@@ -1,4 +1,25 @@
 function toggleMenu() {
   const navLinks = document.querySelector(".nav-links");
   navLinks.classList.toggle("active");
+  document.querySelector(".menu-toggle").classList.toggle("active");
+
+  if (navLinks.classList.contains("active")) {
+    setTimeout(() => {
+      document.addEventListener("click", handleOutsideClick);
+    }, 500);
+  } else {
+    document.removeEventListener("click", handleOutsideClick);
+  }
+}
+
+function handleOutsideClick(event) {
+  const navLinks = document.querySelector(".nav-links");
+  const menuToggle = document.querySelector(".menu-toggle");
+
+  if (!navLinks.contains(event.target)) {
+    navLinks.classList.remove("active");
+    menuToggle.classList.remove("active");
+
+    document.removeEventListener("click", handleOutsideClick);
+  }
 }
